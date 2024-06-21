dc314419d527a6e25d20eb644934fd976131df1f
Test Git <git@publicplan.de>
2024-06-21 17:11:58 +0200

    members module implemented using news extension

--- packages/dengram_package/ext_localconf.php
+++ packages/dengram_package/ext_localconf.php
@@ -4,6 +4,7 @@ defined('TYPO3') or die('Access denied.');
  * Add default RTE configuration
  */
 $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['dengram_package'] = 'EXT:dengram_package/Configuration/RTE/Default.yaml';
+$GLOBALS['TYPO3_CONF_VARS']['FE']['cacheHash']['excludedParameters'][] = '^tx_news_pi1[search]';
 
 /***************
  * PageTS
